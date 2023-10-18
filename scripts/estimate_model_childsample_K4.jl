b14e8ed3df74b0c51daba68136c6758c3b3a6eb7
Joseph Mullins <josephlyonmullins@gmail.com>
2023-10-18 09:05:42 -0500

    four types

--- scripts/estimate_model_childsample_K4.jl
+++ scripts/estimate_model_childsample_K4.jl
@@ -30,5 +30,5 @@ shuffle!(MD)
 p = expectation_maximization(p,EM,MD,n_idx; max_iter = 5, mstep_iter = 20,save = true)
 p = expectation_maximization(p,EM,MD,n_idx;mstep_iter = 100,save = true)
 
-basic_model_fit(p,EM,MD,data,n_idx,"model_stats_childsample.csv")
-savepars_vec(p,"est_childsample")
+basic_model_fit(p,EM,MD,data,n_idx,"model_stats_childsample_K4.csv")
+savepars_vec(p,"est_childsample_K4")
