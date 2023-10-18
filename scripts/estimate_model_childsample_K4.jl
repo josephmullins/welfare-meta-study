01888027e3863baddee78ad555daf400e58541ff
Joseph Mullins <josephlyonmullins@gmail.com>
2023-10-18 09:05:19 -0500

    4 types

---
+++ scripts/estimate_model_childsample_K4.jl
@@ -0,0 +1,34 @@
+include("../src/model.jl")
+include("../src/estimation.jl")
+
+Kτ = 4 #
+Kη = 5 #?
+p = pars(Kτ,Kη)
+p = update_transitions(p)
+nests = get_nests()
+p = (;p...,nests)
+
+x0 = pars_inv(p)
+
+scores = CSV.read("../Data/Data_child_prepped.csv",DataFrame,missingstring = "NA")
+panel = CSV.read("../Data/Data_prepped.csv",DataFrame,missingstring = "NA")
+#select!(panel,Not(:case_idx)) #<- have to add this to other script or re-clean data
+
+# we have to split the panel and then put it back together again
+sipp = @subset panel :source.=="SIPP"
+panel = @chain scores begin
+    @select :id :source
+    innerjoin(panel,on=[:id,:source])
+    vcat(sipp)
+end
+
+MD,EM,data,n_idx = estimation_setup(panel);
+
+Random.seed!(2020)
+shuffle!(MD)
+
+p = expectation_maximization(p,EM,MD,n_idx; max_iter = 5, mstep_iter = 20,save = true)
+p = expectation_maximization(p,EM,MD,n_idx;mstep_iter = 100,save = true)
+
+basic_model_fit(p,EM,MD,data,n_idx,"model_stats_childsample.csv")
+savepars_vec(p,"est_childsample")
