1490b0c486e513f25a81e4c03b1f76c6f15397c2
Joseph Mullins <55055971+josephmullins@users.noreply.github.com>
2023-10-20 10:18:25 -0500

    model fit

--- scripts/checkfit_exante.jl
+++ scripts/checkfit_exante.jl
@@ -3,13 +3,13 @@ include("../src/model.jl")
 include("../src/estimation.jl")
 include("../src/estimation/statistics_exante.jl") #<- overwrites some of the functions in statistics.jl
 
-Kτ = 4 #
-Kη = 5
+Kτ = 3 #
+Kη = 6
 p = pars(Kτ,Kη)
 nests = get_nests()
 p = (;p...,nests)
 
-p = loadpars_vec(p,"est_childsample_K4")
+p = loadpars_vec(p,"est_childsample")
 
 scores = CSV.read("../Data/Data_child_prepped.csv",DataFrame,missingstring = "NA")
 panel = CSV.read("../Data/Data_prepped.csv",DataFrame,missingstring = "NA")
