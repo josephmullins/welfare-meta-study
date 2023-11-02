093913500598103c04058d506496adf43ec0b114
Joseph Mullins <josephlyonmullins@gmail.com>
2023-11-02 10:24:45 -0500

    new ests, updated childcare fit

--- scripts/check_childcare_fit.jl
+++ scripts/check_childcare_fit.jl
@@ -32,46 +32,25 @@ shuffle!(MD)
 
 forward_back_threaded!(p,EM,MD,data,n_idx)
 
-chunks = Iterators.partition(MD,length(MD) ÷ Threads.nthreads())
-chunks = Iterators.partition(MD,length(MD) ÷ 2)
-R = eltype(p.αH)
-tasks = map(chunks) do chunk
-    Threads.@spawn log_likelihood_chunk(p,R,chunk,EM,data,n_idx)
-end
-ll = fetch.(tasks)
-
-break
-
-block = [:αH,:αF]
-ft = [1,1]
-p2 = mstep_major_block(p,block,ft,EM,MD,n_idx,10)
-
-block = [:αH,:αF,:σ]
-ft = [1,1,2]
-p2 = mstep_major_block(p,block,ft,EM,MD,n_idx,100)
-
-block = [:αH,:αF,:βw,:βf]
-ft = [1,1,1,1]
-p3 = mstep_major_block(p2,block,ft,EM,MD,n_idx,100)
-
-
 break
 
 # a couple of functions for getting the childcare fit.
 function childcare_fit(p,EM::Vector{EM_data},MD,data::Vector{likelihood_data},n_idx)
-    #(;V,vj,logP) = get_model(p)
-    D = DataFrame(source = [],arm = [],year=[],Q=[],F = [], est_sample = [],app_status = [])
+    (;V,vj,logP) = get_model(p)
+    D = DataFrame(source = [],arm = [],F = [], est_sample = [],app_status = [])
     for md in MD
-        #solve!(logP,V,vj,p,md)
+        solve!(logP,V,vj,p,md)
         #println(md.case_idx)
         for n in n_idx[md.case_idx]
-            d = childcare_fit(p,[],EM[n],md,data[n])
+            d = childcare_fit(p,logP,EM[n],md,data[n])
             #d[!,:n_idx] .= n
-            D = [D;d]
+            if !isnothing(d)
+                D = [D;d]
+            end
         end
     end
     D = D[.!isnan.(D.F),:]
-    d = combine(groupby(D,[:source,:arm,:app_status,:est_sample,:year,:Q]),:F => Statistics.mean => :F)
+    d = combine(groupby(D,[:source,:arm,:app_status,:est_sample]),:F => Statistics.mean => :F)
 
     return d
 end
@@ -80,27 +59,25 @@ function f_work(s_idx,s_inv)
     _,_,_,H,_ = j_inv(j)
     return H>0
 end
+function paid_care(s_idx,t,logP,s_inv,k_inv)
+    j,k = Tuple(s_inv[s_idx])
+    _,kη,_,_ = Tuple(k_inv[k])
+    EF = 0.
+    norm = 0.
+    for j in choice_set(kη>1)
+        _,_,_,H,F = j_inv(j)
+        norm += H * exp(logP[j,k,t])
+        EF += F * exp(logP[j,k,t])
+    end
+    return EF / norm
+end
+# next: update paid care to be an expected value:
 
 function childcare_fit(p,logP,em::EM_data,md::model_data,data::likelihood_data)
-    T = size(em.q_s,2)
-    Q = 0:T-1
-    N = zeros(T)
-    F = zeros(T)
-    #H = zeros(T)
-    
     k_inv = CartesianIndices((2,p.Kη,md.Kω,p.Kτ))
     K = prod(size(k_inv))
     s_inv = CartesianIndices((9,K))
 
-    # year = md.y0 .+ fld.(md.q0-1 .+ (0:T-1),4)
-    # Q = mod.(md.q0-1 .+ (0:T-1),4) .+ 1
-    year = md.y0 .+ fld.(md.q0 .+ (0:T-1),4) #<- start with one quarter delay in this model.
-    Q = mod.(md.q0 .+ (0:T-1),4) #<- as above
-    for t in 1:T
-        F[t] = em_mean(em.q_s,t,s->paid_care(s,s_inv),s->f_work(s,s_inv))
-        #H[t] = em_mean(em.q_s,t,s->f_work(s,s_inv))
-    end
-    keep = .!data.choice_missing
     if md.source=="MFIP"
         app_status = 3 - 2*data.X_type[5] - data.X_type[6]
     elseif md.source=="FTP" || md.source=="CTJF"
@@ -108,7 +85,16 @@ function childcare_fit(p,logP,em::EM_data,md::model_data,data::likelihood_data)
     else
         app_status = 0
     end
-    return DataFrame(source = md.source, arm = md.arm, year = year[keep], Q = Q[keep], F = F[keep],est_sample = data.use,app_status = app_status)
+
+    t = findfirst(data.chcare_valid)
+    if !isnothing(t)
+        #F = em_mean(em.q_s,t,s->paid_care(s,s_inv),s->f_work(s,s_inv))
+        F = em_mean(em.q_s,t,s->paid_care(s,t,logP,s_inv,k_inv),s->f_work(s,s_inv))
+        return DataFrame(source = md.source, arm = md.arm, F = F,est_sample = data.use,app_status = app_status)
+    else
+        return nothing
+    end
+    
 end
 
 d = childcare_fit(p,EM,MD,data,n_idx)
