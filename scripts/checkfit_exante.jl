5a83e1d8f38d74f5fcd5e5f6f746a51849a5054c
Joseph Mullins <55055971+josephmullins@users.noreply.github.com>
2023-11-09 14:52:34 -0600

    update checkfit

--- scripts/checkfit_exante.jl
+++ scripts/checkfit_exante.jl
@@ -2,14 +2,14 @@
 include("../src/model.jl")
 include("../src/estimation.jl")
 
-Kτ = 5 #
-Kη = 4
+Kτ = 2 #
+Kη = 5
 p = pars(Kτ,Kη)
 nests = get_nests()
 p = (;p...,nests)
 
-p = loadpars_vec(p,"current_est")
-#p = loadpars_vec(p,"est_childsample_K5")
+#p = loadpars_vec(p,"current_est")
+p = loadpars_vec(p,"est_FTP_K2")
 
 scores = CSV.read("../Data/Data_child_prepped.csv",DataFrame,missingstring = "NA")
 panel = CSV.read("../Data/Data_prepped.csv",DataFrame,missingstring = "NA")
