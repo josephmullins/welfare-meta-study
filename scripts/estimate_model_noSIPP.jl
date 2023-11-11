5c29af18a451a78857e0d6a718ea7a1e50aec4ab
Professor Joseph Mullins <mullinsj@ln1002.mangi.msi.umn.edu>
2023-11-11 11:48:47 -0600

    new estimates

--- scripts/estimate_model_noSIPP.jl
+++ scripts/estimate_model_noSIPP.jl
@@ -1,7 +1,7 @@
 include("../src/model.jl")
 include("../src/estimation.jl")
 
-Kτ = 4 #
+Kτ = 3 #
 Kη = 5 #?
 p = pars(Kτ,Kη)
 p = update_transitions(p)
@@ -29,7 +29,7 @@ shuffle!(MD)
 #p = expectation_maximization(p,EM,MD,n_idx; max_iter = 4, mstep_iter = 20,save = true)
 
 MD = MD[[md.source!="SIPP" for md in MD]]
-p = expectation_maximization(p,EM,MD,n_idx;max_iter = 50,mstep_iter = 120,save = true)
+p = expectation_maximization(p,EM,MD,n_idx;max_iter = 15,mstep_iter = 120,save = true)
 #basic_model_fit(p,EM,MD,data,n_idx,"model_stats_childsample_K2.csv")
-savepars_vec(p,"est_noSIPP_K4")
-d = exante_model_fit(pF,EM,mdFTP,data,n_idx,"modelfit_exante_noSIPP.csv")
+savepars_vec(p,"est_noSIPP_K3")
+d = exante_model_fit(p,EM,mdFTP,data,n_idx,"modelfit_exante_noSIPP.csv")
