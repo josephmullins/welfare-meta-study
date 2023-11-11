8f8ebf6871b769922cb5ae1dafed06ec320688b6
Joseph Mullins <josephlyonmullins@gmail.com>
2023-11-11 05:12:07 -0600

    production estimates

--- scripts/estimate_production_mle.jl
+++ scripts/estimate_production_mle.jl
@@ -11,8 +11,8 @@ p = pars(Kτ,Kη)
 nests = get_nests()
 p = (;p...,nests)
 
-#p = loadpars_vec(p,"est_childsample_K5")
-p = loadpars_vec(p,"current_est")
+p = loadpars_vec(p,"est_childsample_K4")
+#p = loadpars_vec(p,"current_est")
 
 
 scores = CSV.read("../Data/Data_child_prepped.csv",DataFrame,missingstring = "NA")
