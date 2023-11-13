760b01d5cc2f311db9fc47df9844964d0e55e327
Joseph Mullins <josephlyonmullins@gmail.com>
2023-11-13 11:15:03 -0600

    code to estimate production

--- scripts/estimate_production.jl
+++ scripts/estimate_production.jl
@@ -5,13 +5,13 @@ include("../src/estimation/production.jl")
 include("../src/estimation/FactorAnalysis.jl")
 
 
-Kτ = 4 #
-Kη = 5
+Kτ = 5 #
+Kη = 4
 p = pars(Kτ,Kη)
 nests = get_nests()
 p = (;p...,nests)
 
-p = loadpars_vec(p,"est_childsample_K4")
+p = loadpars_vec(p,"est_childsample_K5")
 
 
 scores = CSV.read("../Data/Data_child_prepped.csv",DataFrame,missingstring = "NA")
@@ -26,13 +26,8 @@ wght = get_weighting_matrix(scores,columns)
 Λ,Σ,D = estimate_system(scores,wght,columns)
 # calculate the factor scores:
 
-th = factor_scores(scores,M,Λ,Σ,D)
-
-#th ./= std(th,dims=1) #<- normalize the scale of the factors (? no need for this?)
-
-# est,sd = factor_analysis(scores,columns)
-# est_y,sd_y = factor_analysis(@subset(scores,:AGEKID.<=8),columns)
-# est_o,sd_o = factor_analysis(@subset(scores,:AGEKID.>8),columns)
+# var = λ^2 + D, so one sd is sqrt of this.
+th = factor_scores(scores,M,Λ,Σ,D) #<- add Standard Errors?
 
 
 panel = CSV.read("../Data/Data_prepped.csv",DataFrame,missingstring = "NA")
@@ -48,75 +43,116 @@ est_data = production_data(panel,p)
 N = length(est_data)
 X = get_X(est_data) #<- get the X variables
 Z = hcat((d.Z for d in est_data)...) #<- get the instruments
+Z = Z'
+
+# project X onto Z
+Xhat = Z * inv(Z' * Z) * Z' * X
 
 num_X = size(est_data[1].X,1)
-x0 = zeros(8+2num_X)
-x0[1:2] .= 0.5
-x0[3:8] .= 0.8
-pd = production_pars(x0,num_X)
-
-nz = size(Z,1)
-zy = reshape(Z*th / N,2nz)
-zx = Z * X / N
-
-# get initial variance
-V = get_variance(th,X,Z,pd) / N
 
 # y = X*β(δ) + η
 # can we write a true likelihood conditional on z?
 # X = Π * Z + ϵ #<- approximate as normal?
 # p(y|Z) = ∑_x (x*)
-@model function gmm_likelihood(zy,zx,V)
-    num_Z,num_X = size(zx)
-    δI ~ filldist(Uniform(0,3), 2) #[Uniform(0,3) for i in 1:2]
-    δθ ~ filldist(Uniform(0.8,1.),2)
-    g ~ filldist(Uniform(-2,2), 2, 2) #[Uniform(-2,2) for i in 1:2, j in 1:2]
-    β ~ filldist(Turing.Flat(),num_X - 48,2) #[Flat() for i in 1:num_X]
-    zxb = zx * get_β(;δI,g,δθ,β)
-    zy ~ MvNormal(reshape(zxb,2num_Z), V)
+# should we be including a control for age in here?
+# also want to eventually include some controls for η
+
+@model function model_likelihood(th,X)
+    num_X = size(X,2)
+    δI ~ Uniform(0,3) #[Uniform(0,3) for i in 1:2]
+    δθ ~ Uniform(0.,1.)
+    g₁ ~ Uniform(-2,2)
+    g₂ ~ Uniform(-2,2) #[Uniform(-2,2) for i in 1:2, j in 1:2]
+    β ~ filldist(Turing.Flat(),num_X - 48) #[Flat() for i in 1:num_X]
+    m = X * get_β_single(;δI,g₁,g₂,δθ,β)
+    σ ~ FlatPos(0) #?
+    th ~ MvNormal(m, I*σ)
+end
+# set the length of each chain
+length_chain = 500
+
+chain_B_mle = sample(model_likelihood(th[:,1],X),NUTS(),MCMCThreads(),length_chain,Threads.nthreads())
+data_B_mle = DataFrame(chain_B_mle)[:,[:δI,:δθ,:g₁,:g₂]]
+data_B_mle[!,:version] .= "mle"
+data_B_mle[!,:skill] .= "Behavioral"
+
+chain_C_mle = sample(model_likelihood(th[:,2],X),NUTS(),MCMCThreads(),length_chain,Threads.nthreads())
+data_C_mle = DataFrame(chain_C_mle)[:,[:δI,:δθ,:g₁,:g₂]]
+data_C_mle[!,:version] .= "mle"
+data_C_mle[!,:skill] .= "Cognitive"
+
+chain_B_iv = sample(model_likelihood(th[:,1],Xhat),NUTS(),MCMCThreads(),length_chain,Threads.nthreads())
+data_B_iv = DataFrame(chain_B_iv)[:,[:δI,:δθ,:g₁,:g₂]]
+data_B_iv[!,:version] .= "iv"
+data_B_iv[!,:skill] .= "Behavioral"
+
+
+chain_C_iv = sample(model_likelihood(th[:,2],Xhat),NUTS(),MCMCThreads(),length_chain,Threads.nthreads())
+data_C_iv = DataFrame(chain_C_iv)[:,[:δI,:δθ,:g₁,:g₂]]
+data_C_iv[!,:version] .= "iv"
+data_C_iv[!,:skill] .= "Cognitive"
+
+# save these results:
+CSV.write("output/production_ests.csv",[data_B_mle;data_C_mle]) #;data_B_iv;data_C_iv])
+
+function get_β_hetero(;δI,g₁,g₂,δθ,β,Kτ)
+    num_inputs = 1 + 2Kτ
+    β_ = zeros(eltype(δI),num_inputs*16)
+    for t in 1:16
+        d = δθ ^ (t-1)
+        β_[(t-1)*num_inputs+1] = d*δI
+        β_[((t-1)*num_inputs+2):((t-1)*num_inputs+1+Kτ)] .= d*g₁
+        β_[((t-1)*num_inputs+2+Kτ):((t-1)*num_inputs+num_inputs)] .= d*g₂
+    end
+    return [β_;β]
 end
 
-chain = sample(gmm_likelihood(zy,zx,V),NUTS(),200)
-# get first stage estimates:
-est1 = (δI = mean(group(chain,:δI)).nt.mean,
-    g = reshape(mean(group(chain,:g)).nt.mean,2,2),
-    δθ = mean(group(chain,:δθ)).nt.mean, #<- fix this
-    β = reshape(mean(group(chain,:β)).nt.mean,num_X,2)
-)
-V = get_variance(th,X,Z,est1) / N
+function get_X_hetero(est_data,Kτ)
+    N = length(est_data)
+    X = zeros(N,(2Kτ+1)*16)
+    for n in eachindex(est_data)
+        # extract a dummy for xk
+        xk = [sum(est_data[n].X[k:Kτ:(3Kτ)]) for k in 1:Kτ]
+        # then simplify the dummy because this is too slow.
+        #xk = [sum(xk[1:2]),sum(xk[3:4])] #
 
-chain = sample(gmm_likelihood(zy,zx,V),NUTS(),1000)
-
-chain_data = chain.value.data[:,1:8,1]
-names = [:Ib,:Ic,:Db,:Dc,:gNb,:gNc,:gFb,:gFc]
-d = DataFrame(Dict((names[i],chain_data[:,i]) for i in 1:8))
-CSV.write("output/prod_ests_chain.csv",d)
-
-# try something else here
-break
-
-@model function gmm_likelihood(zy,zx,V)
-    num_Z,num_X = size(zx)
-    δI ~ filldist(Uniform(0,3), 2) #[Uniform(0,3) for i in 1:2]
-    δθ ~ filldist(Uniform(0.8,1.),2)
-    g ~ filldist(Uniform(-2,2), 2, 2) #[Uniform(-2,2) for i in 1:2, j in 1:2]
-    β ~ filldist(Turing.Flat(),num_X - 48,2) #[Flat() for i in 1:num_X]
-    zxb = zx * get_β(;δI,g,δθ,β)
-    zy ~ MvNormal(reshape(zxb,2num_Z), V)
+        T = est_data[n].T
+        num_inputs = 2Kτ + 1
+        for t in 1:T
+            #X[n,((t-1)*11+1):t*11] .= [est_data[n].logY[T+1-t]; xk .* est_data[n].H[T+1-t]; xk .* est_data[n].F[T+1-t]]
+            X[n,((t-1)*num_inputs+1):t*num_inputs] .= [est_data[n].logY[T+1-t]; xk .* est_data[n].H[T+1-t]; xk .* est_data[n].F[T+1-t]]
+        end
+    end
+    Xc = hcat((d.X for d in est_data)...)'
+    return [X Xc[:,1:12]]
 end
 
-chain_1 = sample(gmm_likelihood(zy,zx,V),NUTS(),200)
-# get first stage estimates:
-est2 = (δI = mean(group(chain_1,:δI)).nt.mean,
-    g = reshape(mean(group(chain_1,:g)).nt.mean,2,2),
-    δθ = mean(group(chain_1,:δθ)).nt.mean, #<- fix this
-    β = reshape(mean(group(chain_1,:β)).nt.mean,num_X,2)
-)
-V = get_variance(th,X,Z,est2) / N
+X = get_X_hetero(est_data,Kτ) 
 
-chain_2 = sample(gmm_likelihood(zy,zx,V),NUTS(),2000)
 
-chain_data = chain_2.value.data[:,1:8,1]
-names = [:Ib,:Ic,:Db,:Dc,:gNb,:gNc,:gFb,:gFc]
-d = DataFrame(Dict((names[i],chain_data[:,i]) for i in 1:8))
-CSV.write("../output/prod_ests_chain.csv",d)
+@model function model_hetero(th,X,Kτ)
+    num_inputs = 2Kτ + 1
+    num_X = size(X,2)
+    δI ~ Uniform(0,3) #[Uniform(0,3) for i in 1:2]
+    δθ ~ Uniform(0.,1.)
+    g₁ ~ filldist(Uniform(-2,2),Kτ)
+    g₂ ~ filldist(Uniform(-2,2),Kτ) #[Uniform(-2,2) for i in 1:2, j in 1:2]
+    β ~ filldist(Turing.Flat(),num_X - num_inputs*16) #[Flat() for i in 1:num_X]
+    m = X * get_β_hetero(;δI,g₁,g₂,δθ,β,Kτ)
+    σ ~ FlatPos(0) #?
+    th ~ MvNormal(m, I*σ)
+end
+
+vnames = [["g₁[" * string(k) * "]" for k in 1:Kτ];["g₂[" * string(k) * "]" for k in 1:Kτ]]
+vnames = [["δI","δθ"];vnames]
+
+chain_B_hetero = sample(model_hetero(th[:,1],X,Kτ),NUTS(),MCMCThreads(),length_chain,Threads.nthreads())
+data_B_hetero = DataFrame(chain_B_hetero)[:,names]
+data_B_hetero[!,:skill] .= "Behavioral"
+
+chain_C_hetero = sample(model_hetero(th[:,2],X,Kτ),NUTS(),MCMCThreads(),length_chain,Threads.nthreads())
+data_C_hetero = DataFrame(chain_C_hetero)[:,names]
+data_C_hetero[!,:skill] .= "Cognitive"
+
+CSV.write("output/production_ests_hetero.csv",[data_B_hetero;data_C_hetero])
+
