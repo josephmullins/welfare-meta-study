7c93659988f351c5d353b8c45ef5b69446ebb16b
Joseph Mullins <55055971+josephmullins@users.noreply.github.com>
2023-11-27 14:07:54 -0600

    updates to solution method. working toward low memory likelihood.

---
+++ src/estimation/likelihood_alt.jl
@@ -0,0 +1,233 @@
+# define the data that we're going to use:
+using .Threads, Random
+
+function log_likelihood_threaded(p,R::DataType,EM::Vector{EM_data},MD::Vector{model_data},data::Vector{likelihood_data},n_idx)
+    chunks = Iterators.partition(MD,length(MD) ÷ Threads.nthreads())
+    tasks = map(chunks) do chunk
+        Threads.@spawn log_likelihood_chunk(p,R,chunk,EM,data,n_idx)
+    end
+    ll = fetch.(tasks)
+    return sum(ll)
+end
+function log_likelihood_threaded(x,p,EM::Vector{EM_data},MD::Vector{model_data},data::Vector{likelihood_data},n_idx)
+    p = pars(x,p)
+    R = eltype(x)
+    return log_likelihood_threaded(p,R,EM,MD,data,n_idx)
+end
+function log_likelihood_threaded(x,p,fname::Vector{Symbol},ft::Vector{Int64},EM::Vector{EM_data},MD::Vector{model_data},data::Vector{likelihood_data},n_idx)
+    p = pars(x,p,fname,ft)
+    R = eltype(x)
+    return log_likelihood_threaded(p,R,EM,MD,data,n_idx)
+end
+
+
+# writes each answer to a vector LL, for calculating the score
+function log_likelihood_n(x,p,EM::Vector{EM_data},MD::Vector{model_data},data::Vector{likelihood_data},n_idx)
+    N = sum(length(n_idx[md.case_idx]) for md in MD) #<-?
+    p = pars_full(x,p)
+    LL = zeros(eltype(x),N)
+    @threads for md in MD
+        (;vj,V,logP) = get_model(p)
+        solve!(logP,V,vj,p,md)
+        logπτ = zeros(eltype(x),p.Kτ)
+        k_inv = CartesianIndices((2,p.Kη,md.Kω,p.Kτ))
+        K = prod(size(k_inv))
+        s_inv = CartesianIndices((9,K))
+        for n ∈ n_idx[md.case_idx]
+            if data[n].use
+                LL[n] = log_likelihood(EM[n],md,p,logP,data[n])
+                @views LL[n] += log_likelihood_type(logπτ,p.βτ[md.type_block,:],EM[n],data[n],s_inv,k_inv)
+                LL[n] += log_likelihood_η0_full(p,md,EM[n],s_inv,k_inv) #<- probably still a rank issue here
+            end
+        end
+    end
+    return LL
+end
+
+function log_likelihood_chunk(p,R::DataType,MD,EM::Vector{EM_data},data::Vector{likelihood_data},n_idx)
+    # setup data:
+    T = 18*4
+    K = 2 * p.Kη * p.Kτ * 9 #<- maximal state size
+    logP = zeros(R,9,K)
+    V = zeros(R,K,2)
+    vj = zeros(R,J)
+    ll = 0.
+    for md in MD
+        (;B,C) = p.nests
+        T = max((17-md.ageyng)*4,md.T)
+        K = 2 * p.Kη * p.Kτ * md.Kω
+        fill!(V,0.)
+        k_inv = CartesianIndices((2,p.Kη,md.Kω,p.Kτ))
+        k_idx = LinearIndices((2,p.Kη,md.Kω,p.Kτ))
+        state_idx = (;K,k_inv,k_idx)
+        tnow = 1
+        for t in reverse(1:T)
+            iterate!(logP,V,vj,p,md,t,tnow,state_idx)
+            for n ∈ n_idx[md.case_idx]
+                if data[n].use
+                    ll += log_likelihood(EM[n],md,p,logP,data[n])
+                end
+            end
+            tnow = 3 - tnow
+        end
+    end
+    return ll
+end
+
+
+function log_likelihood(em::EM_data,md::model_data,p,logP,data::likelihood_data)
+    J = 9 #<- pass as parameter instead?
+    k_inv = CartesianIndices((2,p.Kη,md.Kω,p.Kτ))
+    K = prod(size(k_inv))
+    s_inv = CartesianIndices((J,K))
+
+    ll = 0.
+    ll += log_likelihood_choices(em,data.t0,logP,s_inv)
+    ll += log_likelihood_transitions(em,p,md,data,k_inv,s_inv)
+    ll += prices_log_like(em,p,md,data,J,s_inv,k_inv)
+    return ll
+end
+
+
+# - wage and childcare log likelihood routines
+function wage_log_like(logW,p,md::model_data,kτ::Int64,kη::Int64,t::Int64)
+    return -0.5 * ((logW - logwage(p,md,kτ,kη,t)) / p.σ_W)^2 - log(p.σ_W)
+end
+
+# - childcare price log-likelihood
+# one (potential issue): no correlation between observed measurement error and whether>0. Surely correlation?
+function chcare_log_like(chcare,p,md::model_data,kτ::Int64,t::Int64)
+    logPF = logpriceF(p,md,kτ,t)
+    resid = chcare - logPF
+    ll = -0.5 * (resid / p.σ_PF)^2 - log(p.σ_PF)
+    return ll
+end
+
+# function chcare_log_like(p,MD::Vector{model_data},EM::Vector{EM_data},data::Vector{likelihood_data},n_idx)
+#     ll = 0.
+#     for md in MD
+#         k_inv = CartesianIndices((2,p.Kη,md.Kω,p.Kτ))
+#         K = prod(size(k_inv))
+#         s_inv = CartesianIndices((J,K))
+    
+#         for n in n_idx[md.case_idx]
+#             em = EM[n]
+#             t0 = data[n].t0
+#             for t in eachindex(data[n].logW)    
+#                 if data[n].chcare_valid[t] && data[n].pay_care[t]
+#                     for s in nzrange(em.q_s,t)
+#                         s_idx = em.q_s.rowval[s]
+#                         wght = em.q_s.nzval[s]
+#                         _,k = Tuple(s_inv[s_idx])
+#                         _,_,_,kτ = Tuple(k_inv[k]) 
+#                         llW = chcare_log_like(data[n].chcare[t],p,md,kτ,t+t0)
+#                         ll += wght * llW
+#                     end
+#                 end
+#             end
+#         end
+#     end
+#     return ll
+# end
+
+function log_likelihood_η0(p,em::EM_data,s_inv,k_inv)
+    ll = 0.
+    for s in nzrange(em.q_s,1)
+        s_idx = em.q_s.rowval[s]
+        _,k = Tuple(s_inv[s_idx])
+        _,kη,_,kτ = Tuple(k_inv[k])
+        wght = em.q_s.nzval[s]
+        llk = log(p.πₛ[kη,kτ])
+        ll += llk*wght
+    end
+    return ll
+end
+
+function log_likelihood_η0_full(p,md::model_data,em::EM_data,s_inv,k_inv)
+    ll = 0.
+    for s in nzrange(em.q_s,1)
+        s_idx = em.q_s.rowval[s]
+        _,k = Tuple(s_inv[s_idx])
+        kA,kη,_,kτ = Tuple(k_inv[k])
+        wght = em.q_s.nzval[s]
+        loc = (md.source=="FTP") + 2(md.source=="CTJF") + 3(md.source=="MFIP") + 4(md.source=="SIPP")
+        llk = log(p.πη[kA,kη,kτ,loc])
+        ll += llk*wght
+    end
+    return ll
+end
+
+
+
+# now do as above but for data coming from an EM algorithm:
+# - here the choices and the states are partially unobserved
+# UPDATE THESE.
+# have only parameters
+function log_likelihood_choices(em::EM_data,t0,logP,s_inv)
+    ll = 0.
+    for t in axes(em.q_s,2)
+        for s in nzrange(em.q_s,t)
+            s_idx = em.q_s.rowval[s]
+            j,k = Tuple(s_inv[s_idx])
+            wght = em.q_s.nzval[s]
+            ll += wght * logP[j,k,t+t0]
+        end
+    end
+    return ll
+end
+
+function log_likelihood_transitions(em::EM_data,p,md::model_data,data::likelihood_data,k_inv,s_inv)
+    ll = 0.
+    Kη = size(k_inv,2)
+    t0 = data.t0
+    for t in eachindex(em.q_ss)
+        for s in nzrange(em.q_s,t)
+            s_idx = em.q_s.rowval[s]
+            j,k = Tuple(s_inv[s_idx])
+            _,A,_,_,_ = j_inv(j)
+            WR = md.R*A
+            unemp = md.unemp[t+t0]
+            _,kη,_,kτ = Tuple(k_inv[k])
+            for sn in nzrange(em.q_ss[t],s_idx)
+                sn_idx = em.q_ss[t].rowval[sn]
+                _,kn = Tuple(s_inv[sn_idx])
+                _,kη_next,_,_ = Tuple(k_inv[kn])
+                f_ss = fη(kη_next,kη,kτ,WR,unemp,p)
+                wght = em.q_ss[t].nzval[sn]
+                #@show t, k, kη, kη_next, wght, f_ss
+                ll += wght * log(f_ss)
+            end
+        end
+    end
+    return ll
+end
+
+function prices_log_like(em::EM_data,p,md::model_data,data::likelihood_data,J::Int64,s_inv,k_inv)
+    ll = 0.
+    t0 = data.t0
+    for t in eachindex(data.logW)
+        if data.wage_valid[t]
+            logW = data.logW[t]
+            for s in nzrange(em.q_s,t)
+                s_idx = em.q_s.rowval[s]
+                wght = em.q_s.nzval[s]
+                _,k = Tuple(s_inv[s_idx])
+                _,kη,_,kτ = Tuple(k_inv[k]) 
+                llW = wage_log_like(logW,p,md,kτ,kη,t+t0)
+                ll += wght * llW
+            end
+        end
+        if md.source=="SIPP" && data.chcare_valid[t] && data.pay_care[t]
+            #logP = data.log_chcare[t]
+            for s in nzrange(em.q_s,t)
+                s_idx = em.q_s.rowval[s]
+                wght = em.q_s.nzval[s]
+                _,k = Tuple(s_inv[s_idx])
+                _,_,_,kτ = Tuple(k_inv[k]) 
+                llW = chcare_log_like(data.log_chcare[t],p,md,kτ,t+t0)
+                ll += wght * llW
+            end
+        end
+    end
+    return ll
+end
--- src/model/solve.jl
+++ src/model/solve.jl
@@ -1,37 +1,45 @@
 #I'm thinking: a named tuple for all the indexing,
 #and a named tuple for all of the parameters?
+
 function solve!(logP,V,vj,p,md::model_data)
-    (;B,C) = p.nests
     T = max((17-md.ageyng)*4,md.T)
     K = 2 * p.Kη * p.Kτ * md.Kω
     fill!(V,0.)
     k_inv = CartesianIndices((2,p.Kη,md.Kω,p.Kτ))
     k_idx = LinearIndices((2,p.Kη,md.Kω,p.Kτ))
+    state_idx = (;K,k_idx,k_inv)
     tnow = 1
     for t in reverse(1:T)
-        tnext = 3-tnow #<- tnow = 1 => tnext =2, tnow=2 => tnext =1
-        for k in 1:K
-            kA,kη,kω,kτ = Tuple(k_inv[k])
-            job_offer = kη>1
-            age_yng = md.ageyng + fld(t,4)
-            eligible = (kω<md.Kω || !md.TL) && (age_yng<18) #<- change what we mean by eligible
-            state = (;kA,kη,kω,kτ,k_idx)
-            if job_offer
-                for j in 1:9
-                    @views vj[j] = calc_vj(j,V[:,tnext],md,state,p,t,eligible)
-                end
-                # choice probs:
-                @views V[k,tnow] = nested_logit(logP[:,k,t],vj;B,C,σ = p.σ) #<- or something like it.
-            else
-                for j in (1,4,7)
-                    @views vj[j] = calc_vj(j,V[:,tnext],md,state,p,t,eligible)
-                end
-                @views V[k,tnow] = plain_logit(logP[:,k,t],vj;B = (1,4,7),σ = p.σ[3])
-            end
-        end
-        tnow = tnext
+        @views iterate!(logP[:,:,t],V,vj,p,md,t,tnow,state_idx)
+        tnow = 3-tnow
     end
     return nothing
+end
+
+function iterate!(logP,V,vj,p,md,t,tnow,state_idx)
+    (;B,C) = p.nests
+    (;K,k_idx,k_inv) = state_idx
+
+    tnext = 3-tnow #<- tnow = 1 => tnext =2, tnow=2 => tnext =1
+    for k in 1:K
+        kA,kη,kω,kτ = Tuple(k_inv[k])
+        job_offer = kη>1
+        age_yng = md.ageyng + fld(t,4)
+        eligible = (kω<md.Kω || !md.TL) && (age_yng<18) #<- change what we mean by eligible
+        state = (;kA,kη,kω,kτ,k_idx)
+        if job_offer
+            for j in 1:9
+                @views vj[j] = calc_vj(j,V[:,tnext],md,state,p,t,eligible)
+            end
+            # choice probs:
+            @views V[k,tnow] = nested_logit(logP[:,k],vj;B,C,σ = p.σ) #<- or something like it.
+        else
+            for j in (1,4,7)
+                @views vj[j] = calc_vj(j,V[:,tnext],md,state,p,t,eligible)
+            end
+            @views V[k,tnow] = plain_logit(logP[:,k],vj;B = (1,4,7),σ = p.σ[3])
+        end
+    end
 end
 
 function calc_vj(j,V,md::model_data,state,pars,t,eligible)
@@ -43,7 +51,8 @@ function calc_vj(j,V,md::model_data,state,pars,t,eligible)
     WR = md.R * A #<- indicates whether a work requirement gives a bump to λ₀
     for kη_next in 1:pars.Kη
         k_next = k_idx[kA_next,kη_next,kω_next,kτ]
-        v += pars.β * fη(kη_next,kη,kτ,WR,md.unemp[min(end,t)],p) * V[k_next]
+        fkk = fη(kη_next,kη,kτ,WR,md.unemp[min(end,t)],pars)
+        v += pars.β * fkk * V[k_next]
     end
     return v
 end
