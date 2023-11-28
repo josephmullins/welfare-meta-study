83f8da51eed0c0dddde361d4c787ac9bb3701483
Joseph Mullins <55055971+josephmullins@users.noreply.github.com>
2023-11-28 16:21:55 -0600

    fixing script.

--- scripts/estimate_model_childsample_K4.jl
+++ scripts/estimate_model_childsample_K4.jl
@@ -27,28 +27,7 @@ MD,EM,data,n_idx = estimation_setup(panel);
 Random.seed!(2020)
 shuffle!(MD)
 
-MD = MD[1:100]
-forward_back_threaded!(p,EM,MD,data,n_idx)
-
-
-block = [:αH,:βw,:ση]
-ft = [1,1,2]
-x = pars_inv(p,block,ft)
-break
-# ok, so we know it's coming from the choice probabilities.
-
-include("../src/estimation/likelihood.jl")
-
-log_likelihood_threaded(x,p,block,ft,EM,MD,data,n_idx)
-@time log_likelihood_threaded(x,p,block,ft,EM,MD,data,n_idx)
-
-include("../src/estimation/likelihood_alt.jl")
-
-log_likelihood_threaded(x,p,block,ft,EM,MD,data,n_idx)
-@time log_likelihood_threaded(x,p,block,ft,EM,MD,data,n_idx)
-
-p = expectation_maximization(p,EM,MD,n_idx; max_iter = 4, mstep_iter = 20,save = true)
-#p = expectation_maximization(p,EM,MD,n_idx;max_iter = 150,mstep_iter = 120,save = true)
+p = expectation_maximization(p,EM,MD,n_idx;max_iter = 150,mstep_iter = 20,save = true)
 
 basic_model_fit(p,EM,MD,data,n_idx,"model_stats_childsample_K4.csv")
 savepars_vec(p,"est_childsample_K4")
