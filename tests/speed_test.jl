eb9c5d246d56be580f475662cb5e5270d184c1e2
Joseph Mullins <josephlyonmullins@gmail.com>
2023-11-30 08:43:38 -0600

    update speed test

--- tests/speed_test.jl
+++ tests/speed_test.jl
@@ -30,46 +30,53 @@ shuffle!(MD)
 
 forward_back_threaded!(p,EM,MD,data,n_idx)
 
-block = [:αH,:αA,:βw]
-ft = [1,1,1]
-x0 = pars_inv(p,block,ft)
 
 
-function solve_model_chunk(x,p,MD,EM::Vector{EM_data},data::Vector{likelihood_data},n_idx)
-    # setup data:
-    T = 18*4
-    K = 2 * p.Kη * p.Kτ * 9 #<- maximal state size
-    R = eltype(x)
-    logP = zeros(R,9,K,T)
-    V = zeros(R,K,2)
-    vj = zeros(R,9)
-    ll = 0.
-    for md in MD
-        solve!(logP,V,vj,p,md)
-    end
-    return nothing
-end
+# function solve_model_chunk(x,p,MD,EM::Vector{EM_data},data::Vector{likelihood_data},n_idx)
+#     # setup data:
+#     T = 18*4
+#     K = 2 * p.Kη * p.Kτ * 9 #<- maximal state size
+#     R = eltype(x)
+#     logP = zeros(R,9,K,T)
+#     V = zeros(R,K,2)
+#     vj = zeros(R,9)
+#     ll = 0.
+#     for md in MD
+#         solve!(logP,V,vj,p,md)
+#     end
+#     return nothing
+# end
 
-function solve_threaded(x,p,EM::Vector{EM_data},MD::Vector{model_data},data::Vector{likelihood_data},n_idx)
-    chunks = Iterators.partition(MD,length(MD) ÷ Threads.nthreads())
-    tasks = map(chunks) do chunk
-        Threads.@spawn solve_model_chunk(x,p,chunk,EM,data,n_idx)
-    end
-    ll = fetch.(tasks)
-    return nothing
-end
+# function solve_threaded(x,p,EM::Vector{EM_data},MD::Vector{model_data},data::Vector{likelihood_data},n_idx)
+#     chunks = Iterators.partition(MD,length(MD) ÷ Threads.nthreads())
+#     tasks = map(chunks) do chunk
+#         Threads.@spawn solve_model_chunk(x,p,chunk,EM,data,n_idx)
+#     end
+#     ll = fetch.(tasks)
+#     return nothing
+# end
+
+# solve_threaded(x0,p,EM,MD,data,n_idx)
+# @time solve_threaded(x0,p,EM,MD,data,n_idx)
 
 
-solve_threaded(x0,p,EM,MD,data,n_idx)
-@time solve_threaded(x0,p,EM,MD,data,n_idx)
-
-break
 using ForwardDiff
 function get_G(x,p,block,ft,EM,MD,data,n_idx)
     ForwardDiff.gradient(x->log_likelihood_threaded(x0,p,block,ft,EM,MD,data,n_idx),x)
 end
+block = [:αH,:αA,:βw]
+ft = [1,1,1]
+x0 = pars_inv(p,block,ft)
+
 log_likelihood_threaded(x0,p,block,ft,EM,MD,data,n_idx)
 get_G(x0,p,block,ft,EM,MD,data,n_idx)
+
+@time log_likelihood_threaded(x0,p,block,ft,EM,MD,data,n_idx)
+@time get_G(x0,p,block,ft,EM,MD,data,n_idx)
+
+block = [:μₒ,:σₒ]
+ft = [1,2]
+x0 = pars_inv(p,block,ft)
 
 @time log_likelihood_threaded(x0,p,block,ft,EM,MD,data,n_idx)
 @time get_G(x0,p,block,ft,EM,MD,data,n_idx)
