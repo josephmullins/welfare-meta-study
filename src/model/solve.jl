e1975df471079858fe9d842f8739c86cd2a6b0fd
Joseph Mullins <josephlyonmullins@gmail.com>
2023-11-29 20:29:53 -0600

    fixed bug

--- src/model/solve.jl
+++ src/model/solve.jl
@@ -51,25 +51,25 @@ function calc_vj(j,V,md::model_data,state,pars,t,eligible)
     unemp = md.unemp[min(end,t)]
     if kη==1
         WR = md.R * A #<- indicates whether a work requirement gives a bump to λ₀
-        λ₀ = logit(p.λ₀[kτ] + p.λᵤ*unemp + p.λR*WR)
+        λ₀ = logit(pars.λ₀[kτ] + pars.λᵤ*unemp + pars.λR*WR)
         kη_next = 1
         k_next = k_idx[kA_next,kη_next,kω_next,kτ]
         v += pars.β * (1-λ₀) * V[k_next]
-        for kη_next ∈ 2:p.Kη
+        for kη_next ∈ 2:pars.Kη
             k_next = k_idx[kA_next,kη_next,kω_next,kτ]
-            fkk = λ₀ * p.πₒ[kη_next-1,1]
+            fkk = λ₀ * pars.πₒ[kη_next-1,1]
             v += pars.β * fkk * V[k_next]    
         end
     else
-        λ₁ = logit(p.λ₁[kτ] + p.λᵤ*unemp)
+        λ₁ = logit(pars.λ₁[kτ] + pars.λᵤ*unemp)
         kη_next = 1
         k_next = k_idx[kA_next,kη_next,kω_next,kτ]
-        v += pars.β * p.δ[kτ] * V[k_next]
-        for kη_next ∈ 2:p.Kη
+        v += pars.β * pars.δ[kτ] * V[k_next]
+        for kη_next ∈ 2:pars.Kη
             k_next = k_idx[kA_next,kη_next,kω_next,kτ]
-            fkk = (1 - p.δ[kτ]) * λ₁ * p.πₒ[kη_next-1,kη]
+            fkk = (1 - pars.δ[kτ]) * λ₁ * pars.πₒ[kη_next-1,kη]
             if kη_next==kη
-                fkk += (1 - p.δ[kτ]) * (1-λ₁)
+                fkk += (1 - pars.δ[kτ]) * (1-λ₁)
             end
             v += pars.β * fkk * V[k_next] 
         end
