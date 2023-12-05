22ec58372b83dd0623fe62d8d9bd4dec472cc1ec
Joseph Mullins <josephlyonmullins@gmail.com>
2023-12-05 10:01:16 -0600

    improved the max step

--- scripts/estimate_model_childsample_K5.jl
+++ scripts/estimate_model_childsample_K5.jl
@@ -27,8 +27,7 @@ MD,EM,data,n_idx = estimation_setup(panel);
 Random.seed!(2020)
 shuffle!(MD)
 
-
-p = expectation_maximization(p,EM,MD,n_idx;max_iter = 1,mstep_iter = 2,save = true)
+p = expectation_maximization(p,EM,MD,n_idx;max_iter = 20,mstep_iter = 5,save = true)
 
 basic_model_fit(p,EM,MD,data,n_idx,"model_stats_K5.csv")
 d = exante_model_fit(p,EM,MD,data,n_idx,"modelfit_exante_K5.csv")
